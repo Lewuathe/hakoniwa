a0ea8d1cddc059d1a7ae359f5aecc0b848a752e6
Kai Sasaki <lewuathe@gmail.com>
2023-09-01 16:02:03 +0900

    Apply format

--- hakoniwa/entity/openai_entity.py
+++ hakoniwa/entity/openai_entity.py
@@ -20,7 +20,8 @@ class OpenAIEntity(DynamicEntity):
             messages=[
                 {
                     "role": "system",
-                    "content": f"{self.personality}" + """
+                    "content": f"{self.personality}"
+                    + """
 We expect you to take action each time based on the list of possible action and state you are now. You are given some input from the environment at each iteration align with the current state and action you took.
 Please give us the ID of action you would take and the output text you would return to the environment according to the following JSON format which have an action ID you took in "action" field, and arbitrary output text in "output" field.
 
